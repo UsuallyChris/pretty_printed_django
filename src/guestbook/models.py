fe7840ff41fbab1f15114a7a5a069b1b3066f3ec
UsuallyChris <schoonc@gmail.com>
2019-05-03 19:31:19 -0700

    Added __str__ function to comment model

--- src/guestbook/models.py
+++ src/guestbook/models.py
@@ -6,3 +6,6 @@ class Comment(models.Model):
   name = models.CharField(max_length=20)
   comment = models.TextField()
   date_added = models.DateTimeField(default=timezone.now)
+
+  def __str__(self):
+    return f'<Name: {self.name}, ID: {self.id}>'
